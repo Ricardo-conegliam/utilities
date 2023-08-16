347c5cdcc29ca907ca9f8fab0bb7121c49e97fba
rconegliam@gmail.com <rconegliam@gmail.com>
2023-08-16 12:19:48 +0000

    Small evolution on notebook

--- TABLE MANAGEMENT/SmallFilesCount.py
+++ TABLE MANAGEMENT/SmallFilesCount.py
@@ -1,29 +1,52 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ## Objective : Collect small file stats from given catalog (or all)
+# MAGIC ### To process all the catalogs, just use "*" as parameter
+# MAGIC ### You can run it daily to get the evolution
+# MAGIC ### You can create Queries and Alerts to monitor the smallfiles stats
+# MAGIC ### This scripts only Describe Details, so its not dangerous.  However, if you use AutoOptimize = Y, it will try to optimize all tables with
+# MAGIC ###    numFiles > 1 and avgFileSizeinMB > 50
+# MAGIC ### Do not run with AutoOptimize="Y" if you have process with updates and merges taking places
+# MAGIC ### Works only with Unit Catalog
+# MAGIC ###
+# MAGIC ### Author :  SSA Team, Ricardo Conegliam
+# MAGIC
+
+# COMMAND ----------
+
+# DBTITLE 1,Imports
 from delta import DeltaTable
 from datetime import datetime
 from pyspark.sql.functions import col, lit, round, current_timestamp, coalesce
 
 # COMMAND ----------
 
+# DBTITLE 1,Variables
 dbutils.widgets.text("Catalog","main")
 dbutils.widgets.text("Days_Since_Last_Alt","30")
+dbutils.widgets.text("AutoOptimize","N")
+
 
 catalog = dbutils.widgets.get("Catalog")
 days_since_last_alt = dbutils.widgets.get("Days_Since_Last_Alt")
+auto_optimize = dbutils.widgets.get("AutoOptimize")
 
-## LEGACY MODE IS NOT WORKING, USE ONLY "UC" TYPE
-catalogType = "UC"
 table_file_stats = "main.default.tablefilestats"
+table_file_stats_hist = "main.default.tablefilestats_hist"
+
 verbose = False
 
 now = datetime.now() 
 
-batch_id = now.strftime("%Y%m%d-%H%M%S")
+# It must have only one stats daily
+# If you run twice, the script delete the data with same date (batchId) before insert new data
+batch_id = now.strftime("%Y%m%d")
 
 
 
 # COMMAND ----------
 
+# DBTITLE 1,Creating the tables if they don't exist
 spark.sql(f" \
 CREATE TABLE IF NOT EXISTS {table_file_stats} ( \
   batchId STRING,\
@@ -36,32 +59,37 @@ CREATE TABLE IF NOT EXISTS {table_file_stats} ( \
   timestamp TIMESTAMP \
 )")
 
+spark.sql(f" \
+CREATE TABLE IF NOT EXISTS {table_file_stats_hist} ( \
+  batchId STRING,\
+  catalog STRING, \
+  schema STRING, \
+  table STRING, \
+  numFiles BIGINT, \
+  sizeMb DOUBLE, \
+  avgFileSizeMb DOUBLE, \
+  timestamp TIMESTAMP \
+)")
 # spark.sql(f"insert into {table_file_stats} ({table['table_catalog']},{table['table_schema']},{table['table_name']},)")
 
 # COMMAND ----------
 
+# DBTITLE 1,Creating a function 
 # Note:  Be aware you are not zordering anything!
 
 
 def listSmallfiles(catalog):
 
-    ## Note : if you are sadly not using UC, use this dataframe definition instead ##
-    if catalogType == "LEGACY":
-        df = spark.sql(f"show tables from {catalog}.{schema}").select(
-            col("tableName").alias("table_name"),
-            lit(schema).alias("table_schema"),
-            lit(catalog).alias("table_catalog"),
-        )
-    else:
-        df = (
-            spark.table("system.information_schema.tables")
-            .select("table_catalog", "table_schema", "table_name")
-            .where(f'table_catalog = "{catalog}" ')
-            .where("data_source_format = 'DELTA'")
-            .where("table_catalog <> '__databricks_internal'")
-            .where(f"last_altered > now() - interval {days_since_last_alt} days")
-            .orderBy("table_schema")
-        )
+    df = (
+        spark.table("system.information_schema.tables")
+        .select("table_catalog", "table_schema", "table_name")
+        .where(f'table_catalog = "{catalog}" ')
+        .where("table_catalog <> 'information_schema'")
+        .where("data_source_format = 'DELTA'")
+        .where("table_catalog <> '__databricks_internal'")
+        .where(f"last_altered > now() - interval {days_since_last_alt} days")
+        .orderBy("table_schema")
+    )
 
     tableList = [
         data
@@ -79,7 +107,7 @@ def listSmallfiles(catalog):
         )
 
         if verbose:  
-            print(f"Checking {fullname}...")
+            print(f"    Checking {fullname}...")
 
         try:
 
@@ -101,29 +129,99 @@ def listSmallfiles(catalog):
 
             if dfDetail.count() > 0:
                 if verbose:
-                    print(f"Appending {fullname}")
+                    print(f"        Appending {fullname}")
                 list.append(dfDetail.collect()[0])
         except Exception as e:
             output = f"{e}"
-            print(f"Error on {fullname} {e}")
+            print(f"        Error on {fullname} {e}")
 
     return list
 
 # COMMAND ----------
 
-tablesStats = listSmallfiles(catalog)
+# DBTITLE 1,Saving the data
+def writeDataframe(pcatalog,ptablesStats):
+
+    if ptablesStats:
+
+        if verbose:
+            print(f"Writing data of catalog {pcatalog}")
+
+        df = spark.createDataFrame(tablesStats).withColumn("timestamp", current_timestamp())
+
+        if verbose:
+            display(df.orderBy(col("avgFileSizeMB").asc()))
+
+
+        spark.sql(f"DELETE FROM {table_file_stats} WHERE catalog = '{pcatalog}'")
+        spark.sql(f"DELETE FROM {table_file_stats_hist} WHERE batchId = '{batch_id}'")
+        
+        df.write.mode("append").saveAsTable(table_file_stats)
+        df.write.mode("append").saveAsTable(table_file_stats_hist)
+    
 
 
 # COMMAND ----------
 
-df = spark.createDataFrame(tablesStats).withColumn("timestamp", current_timestamp())
+# DBTITLE 1,Calling the function listSmallfiles to append in list
+if catalog == "*":
 
-if verbose:
-    display(df.orderBy(col("avgFileSizeMB").asc()))
+    catalogs = (
+        spark.sql("SELECT catalog_name FROM system.information_schema.catalogs WHERE catalog_name <> 'system'")
+        .collect()
+    )
 
-df.write.mode("append").saveAsTable(table_file_stats)
+    for catalog_to_analyze in catalogs:
+        if verbose:
+            print(f"Analyzing catalog {catalog_to_analyze['catalog_name']}")
+        tablesStats = listSmallfiles(catalog_to_analyze['catalog_name'])
+        writeDataframe(catalog_to_analyze['catalog_name'],tablesStats)
 
+else:
+    tablesStats = listSmallfiles(catalog)
+    writeDataframe(catalog,tablesStats)
+
+# COMMAND ----------
+
+# DBTITLE 1,Optimizing stats tables
 spark.sql(f"optimize {table_file_stats}")
+spark.sql(f"optimize {table_file_stats_hist}")
+
+# COMMAND ----------
+
+# DBTITLE 1,Optimizing all tables with numFiles > 1 and avgFileSize <50MB
+if auto_optimize == "Y":
+    df = ( 
+          spark.sql(" \
+                    select \
+                            catalog,schema,table,numFiles \
+                    from \
+                            main.default.tablefilestats \
+                    where   \
+                            avgFileSizeMb < 50 \
+                            and \
+                            numFiles > 1 \
+                    order by \
+                            avgFileSizeMb \
+                    ")
+          )
+          
+    tableList = [data for data in df.collect()]
+
+    for table in tableList:
+
+        fullname = f"{table['catalog']}.{table['schema']}.{table['table']}"
+
+        print(f"Running Optimize on {fullname}...")
+
+        try:
+            df = DeltaTable.forName(spark,f"{fullname}")
+            df.optimize().executeCompaction()
+
+        except Exception as e:  
+            output = f"{e}"  
+            print(f"Error on optimizing {fullname} : {e}")
+    
 
 # COMMAND ----------
 
