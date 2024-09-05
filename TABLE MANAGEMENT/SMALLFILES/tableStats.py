8fe03256b4f30646c7f2fbaeb107bf8eafd3f4b9
rconegliam@gmail.com <rconegliam@gmail.com>
2024-09-05 19:03:48 +0000

    Removed unused rows

--- TABLE MANAGEMENT/SMALLFILES/tableStats.py
+++ TABLE MANAGEMENT/SMALLFILES/tableStats.py
@@ -453,18 +453,4 @@ else:
 
 # COMMAND ----------
 
-dbutils.fs.ls("abfss://fe-shared-ssa-latam-unity-catalog@fesharedssalatamstorage1.dfs.core.windows.net/unity_catalog/c1d760bc-fa70-4b42-842b-b826e56c119a/tables/d26c64b8-0178-4681-8c8c-23086b6f7922")
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC alter table main.schema01.struct_fields_table set tblproperties ("delta.dataSkippingNumIndexedCols"=2)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC describe detail main.schema01.struct_fields_table
-
-# COMMAND ----------
-
 
